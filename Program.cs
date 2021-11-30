27be32fded05ec9a9f6d8d33434cdb9c2587fbca
Emily Schwartau <emilyschwartau+1@gmail.com>
2021-11-30 14:59:17 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//make a string
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+
+string name = "Emily";
+Console.WriteLine(name);
+
+//make a number
+//Int (whole number)
+int age = 26;
+Console.WriteLine(age);
+
+//Floating point number
+double example = 2.6e+1D;
+Console.WriteLine(example);
+
+//long - used when int isn't big enough to store number
+long exampleOne = 15000000000L;
+Console.WriteLine(exampleOne);
+
+//float - can also be used for scientific notation
+float exampleFloat = 5.75F;
+Console.WriteLine(exampleFloat);
+
+
+
+
+
+
+
